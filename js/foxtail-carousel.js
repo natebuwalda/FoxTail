8f96b3881aeb5a09f1abe3243722a4f3d89e914b
Nate Buwalda <natebuwalda@gmail.com>
2013-08-11 22:29:27 -0500

    Added initial flexslider implementation, removed old carousel code to keep things clean

--- js/foxtail-carousel.js
+++ js/foxtail-carousel.js
@@ -1,29 +1,5 @@
-$(function() {
-	$('.slider-main')
-	.on('create.jcarousel', initCallback)
-	.jcarousel({
-		'wrap' : 'last'
-	})
-	.jcarouselAutoscroll({
-		'interval' : 5000,
-		'target' : '+=1'
-	});	
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
 });
-
-function initCallback(event) {
-	$('.slider-main').hover(stopAutoScroll, startAutoScroll);
-	$('.slider-link').click(scrollToImage);
-}
-
-function stopAutoScroll() {
-	$('.slider-main').jcarouselAutoscroll('stop');
-}
-
-function startAutoScroll() {
-	$('.slider-main').jcarouselAutoscroll('start');
-}
-
-function scrollToImage() {
-	$('.slider-main').jcarouselAutoscroll('stop');
-	$('.slider-main').jcarousel('scroll', $(this).data('rel'));
-}
