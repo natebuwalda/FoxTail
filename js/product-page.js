cc4705d7daf0ba4509f90c94fae5cc5700aa318a
Nate Buwalda <natebuwalda@gmail.com>
2013-09-08 15:28:25 -0500

    Renamed carousel js files to match their associated page, configured product carousel for thumbnails

---
+++ js/product-page.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+	prevText: "Prev",
+	nextText: "Next",
+	controlNav: "thumbnails",
+	animationLoop: false
+  });
+});
